956633bf8e0486cc1027698d32b8961566bd7716
zetta <zetaweb@gmail.com>
2010-12-31 12:10:16 -0600

    minimum

--- src/theme-options/theme-options-setup.php
+++ src/theme-options/theme-options-setup.php
@@ -1,15 +1,20 @@
 <?php
 
+$options = array();
+$options[] = wpo_tab("Basic Types");
+$options[] = wpo_string('string','this is the default string...','String','You can store strings in this option');
+$options[] = wpo_text('text','default text','Text','Here you can store large text');
+$options[] = wpo_boolean('boolean1',false,'Boolean','True or False');
+$options[] = wpo_boolean('boolean2',true,'Boolean');
+$options[] = wpo_number('number',10,'Number');
+$options[] = wpo_check('check',true,'Check','Toggle Option');
+
+
+
+
+
 setup_options(
 	'http://www.manualurl.com',
 	'http://www.forumurl.com',
-	array(
-		wpo_tab("Basic Types"),
-		  wpo_string('string','this is the default string...','String','You can store strings in this option'),
-		  wpo_text('text','default text','Text','Here you can store large text'),
-		  wpo_boolean('boolean1',false,'Boolean','True or False'),
-		  wpo_boolean('boolean2',true,'Boolean'),
-		  wpo_number('number',10,'Number'),
-		  wpo_check('check',true,'Check','Toggle Option')
-	)
+	$options
 );
