24484958e287061cac81bae04bd3932727596d1b
zetta <zetaweb@gmail.com>
2011-03-03 13:30:21 -0600

    sin comentarios, sigo sin poder escribir tests para wordpress

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ Cool library to generate options in wordpress
 ## Tests ##
 
 Para ejecutar los test es necesario
-- phpunit 
+- Pear
+- XDebug
+- PHPUnit
+- una instalacion de wordpress funcionando (se debe configurar la ruta en el archivo test/bootstrap.php)
+
 
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,12 +3,9 @@
 
 if (!defined('WP_INSTALATION'))
 {
-    define ("WP_INSTALATION",'/var/www/wordpress/');
+    define ("WP_INSTALATION",'/var/www/wordpress');
     set_include_path(get_include_path() . PATH_SEPARATOR . WP_INSTALATION);
-    
-    $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
     require_once "wp-config.php";
-    //error_reporting(-1);
 }
 
 
